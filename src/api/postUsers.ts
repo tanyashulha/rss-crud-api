5071b65ae8e72dc2f180cf508e102c526ceb52f2
tatsiana_shulha <t.shulha@symfa.com>
2025-05-12 13:18:26 +0200

    add object validator

--- src/api/postUsers.ts
+++ src/api/postUsers.ts
@@ -5,15 +5,22 @@ import { callMiddleware } from '../middleware/middleware';
 import { v4 as uuidv4 } from 'uuid';
 import { MessagesEnum } from '../enums/messages.enum';
 import { StatusCodesEnum } from '../enums/status-codes.enum';
+import { validateUserObj } from '../validators/validate-user-obj.validator';
+import { CustomError } from '../utils/custom-error.utils';
 
 export const postUsers = async (req: IncomingMessage, res: ServerResponse) => {
     try {
-        const userObject = await getResponse(req);
-        const updateUser = JSON.parse(JSON.stringify(userObject)) as IUser;
-        const userId = uuidv4();
-        updateUser.id = userId;
-        db.set(updateUser, userId);
-        callMiddleware(res, StatusCodesEnum.CREATE, updateUser);
+        const user = await getResponse(req);
+        const updatedUser = JSON.parse(JSON.stringify(user)) as IUser;
+
+        if (validateUserObj(updatedUser)) {
+            const userId = uuidv4();
+            updatedUser.id = userId;
+            db.set(updatedUser, userId);
+            callMiddleware(res, StatusCodesEnum.CREATE, updatedUser);
+        } else {
+            throw new CustomError(StatusCodesEnum.BAD_REQUEST, MessagesEnum.InvalidDataInRequest);
+        }
     } catch {
         callMiddleware(res, StatusCodesEnum.SERVER_ERROR, { message: MessagesEnum.InternalServerError });
     }
--- src/api/putUserByUserId.ts
+++ src/api/putUserByUserId.ts
@@ -6,19 +6,24 @@ import { validate } from 'uuid';
 import { CustomError } from '../utils/custom-error.utils';
 import { MessagesEnum } from '../enums/messages.enum';
 import { StatusCodesEnum } from '../enums/status-codes.enum';
+import { validateUserObj } from '../validators/validate-user-obj.validator';
 
 export const putUserByUserId = async (id: string, res: ServerResponse, req: IncomingMessage) => {
     try {
         if (validate(id)) {
             if (db.isAlreadyExist(id)) {
-                const user = await getResponse(req);
-                db.put(user as IUser, id);
-                callMiddleware(res, StatusCodesEnum.SUCCESS, user);
+                const user = await getResponse(req) as IUser;
+                if (validateUserObj(user)) {
+                    db.put(user as IUser, id);
+                    callMiddleware(res, StatusCodesEnum.SUCCESS, user);
+                } else {
+                    throw new CustomError(StatusCodesEnum.BAD_REQUEST, MessagesEnum.InvalidDataInRequest);
+                }
             } else {
-                throw new CustomError(StatusCodesEnum.BAD_REQUEST, MessagesEnum.InvalidDataInRequest);
+                throw new CustomError(StatusCodesEnum.NOT_FOUND, MessagesEnum.UserIsNotExsist);
             }
         } else {
-            throw new CustomError(StatusCodesEnum.NOT_FOUND, MessagesEnum.UserIsNotExsist);
+            throw new CustomError(StatusCodesEnum.BAD_REQUEST, MessagesEnum.InvalidDataInRequest);
         }
     } catch {
         callMiddleware(res, StatusCodesEnum.SERVER_ERROR, { message: MessagesEnum.InternalServerError });
